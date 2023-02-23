12428c6aab33a01078c5019f63145f14cbc14e3d
Leeviantila <54677074+Leeviantila@users.noreply.github.com>
2023-02-23 19:32:26 +0200

    router lisätty

--- react_nettisivu/src/App.js
+++ react_nettisivu/src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
 import {Message} from "./pages/Message";
 import {Buttons} from "./pages/Buttons";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Router } from "./pages/Router";
 
 
 function App() {
   return (
-    <div>
-      <Message/>
-      <Buttons/>
+      
+    <BrowserRouter>
+      <div>
+        <Routes>
+              <Route path = '/' element = {<Router/>}>
+              <Route index element = {<Message/>}/>
+              <Route path ='/Buttons' element = {<Buttons/>}/>
+
+            <Route/>
+          </Route>
+
+        </Routes>
+      </div>
+
+    </BrowserRouter>
 
 
-    </div>
   );
 }
 
