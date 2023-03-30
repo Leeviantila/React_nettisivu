65ecae7b8af1628ea4ac338e0cc94ef1f3c4a5aa
Leeviantila <54677074+Leeviantila@users.noreply.github.com>
2023-03-30 19:30:10 +0300

    syöttökentän tekemistä, List scripti

--- react_nettisivu/src/pages/List.jsx
+++ react_nettisivu/src/pages/List.jsx
@@ -8,20 +8,47 @@ export const List = () => {
     const[content, setContent] = useState([]);
     const[inputValue, setInputValue] = useState('');
 
+    function clear(){
+        setContent([]);
+
+    }
     
     return(
         <div>
             <div>
 
                 <form >
-                    <label>Enter your name:
-                        <input 
-                            type="text" placeholder="Tekstikenttä.." required
-                            
-                        />
-                    </label>
-                    <input type="submit" value="Lähetä"/>
+                    <from onSubmit={(event)=>{
+                        event.preventDefault()
+                        const temp = [inputValue, ...content]
+                        setContent(temp);
+                        setInputValue('');
+
+
+                    }}>
+                            <input
+                                type="text" placeholder="Tekstikenttä.." 
+                                value    = {inputValue}
+                                onChange = {e => setInputValue(e.target.value)}
+                                
+                                required/>
+                            <input type="submit" value="Lähetä"/>
+                            {
+                                content.map((item) => (
+                                    
+                                    <ul>
+                                        <li>
+                                            <h2>{item}</h2>
+                                        </li>
+                                    </ul>                            
+                                ))                               
+                            }
+                    </from>
+                    <button onClick={clear}>clear</button>
+
+
                 </form>
+                
 
 
             
